bc2f47298293a4276537eb0c8fc300d38e3077cc
Pam Tholan <pamelatholan@yahoo.com>
2019-06-07 22:37:54 -0400

    added links to bootstrap, firebase, js file, css file, jquery, moment and firebase database

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyC4uyCKiXfx4fEvNmfQyrLHTqM9WHGI6qA",
+    authDomain: "train-schedule-a6872.firebaseapp.com",
+    databaseURL: "https://train-schedule-a6872.firebaseio.com",
+    projectId: "train-schedule-a6872",
+    storageBucket: "train-schedule-a6872.appspot.com",
+    messagingSenderId: "53139023806",
+    appId: "1:53139023806:web:6fd8ed6ce1dfbeea"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
